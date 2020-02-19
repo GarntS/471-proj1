3179a628eb64afdeac20c62e2bad9a1ca42bd3e9
garnt <garnt@ass.is>
2020-02-19 14:39:51 -0500

    Fixed bug regarding move ordering, added basic evaluation function.

--- baby_driver.py
+++ baby_driver.py
@@ -7,7 +7,8 @@
 
 # imports
 import chess
-from eval_funcs import test_eval #TODO(y'all): add any new evaluation functions
+import random
+from eval_funcs import test_eval, eval_countpieces#TODO(y'all): add any new evaluation functions
 
 # minimax() runs an iteration of minimax-ab with the specified max depth
 # @param depth          set to the maximum depth we want to evaluate, decreases
@@ -26,8 +27,13 @@ def minimax(depth, board, alpha, beta, is_maximizing, eval_func):
     if(depth == 0):
         return (-eval_func(board), board.peek())
 
-    # populate the possible moves with the current board state
-    possible_moves = board.legal_moves
+    # populate the possible moves that can be made from the current board state
+    possible_moves = []
+    # iterate through the generator because we can only shuffle a list
+    for move in board.legal_moves:
+        possible_moves.append(move)
+    # shuffle the order of the moves
+    random.shuffle(possible_moves)
 
     # if we're the maximizing player
     if(is_maximizing):
@@ -104,7 +110,7 @@ def play_game(white_eval, white_depth, black_eval, black_depth):
         # if it's black's turn
         else:
             # determine the best move to make via minimax-ab
-            best_pair = minimax(white_depth, board, float("-inf"), float("inf"), False, white_eval)
+            best_pair = minimax(black_depth, board, float("-inf"), float("inf"), False, black_eval)
             # make that move and pass the turn
             board.push(best_pair[1])
 
@@ -119,8 +125,10 @@ def play_game(white_eval, white_depth, black_eval, black_depth):
 # main() is the entry point
 def main():
     # play an example game
-    result = play_game(test_eval, 4, test_eval, 4)
-    print(result)
+    results = []
+    for i in range(10):
+        results.append(play_game(eval_countpieces, 10, eval_countpieces, 1))
+    print(results)
     #TODO(y'all):   Run some tests on a combination of evaluation complexity and
     #               search depth to determine what their effects are. You can
     #               run those games and collect those results here. An example
--- eval_funcs.py
+++ eval_funcs.py
@@ -20,3 +20,52 @@ def test_eval(board):
 # TODO(y'all):  implement two more evaluation functions, one more thorough than
 #               the other. Use the function test_eval() as shown above as a
 #               template.
+
+# piece_to_pts() returns a point evaluation for a given python-chess piece.
+# @param piece  a python-chess piece object of the piece to be evaluated.
+#               NOTE: Cannot be None or it'll break. Catch those elsewhere.
+# @return       the number of points that piece is worth, according to standard
+#               valuations dating back to the 18th century.
+def piece_to_pts(piece):
+    # fetch the symbol for the piece
+    charcode = piece.symbol().lower()
+    # giant if-else block to handle all the pieces
+    if charcode == 'p':
+        return 1
+    if charcode == 'b':
+        return 3
+    if charcode == 'n':
+        return 3
+    if charcode == 'r':
+        return 5
+    if charcode == 'q':
+        return 9
+    if charcode == 'k':
+        return 1
+    return 0
+
+# eval_countpieces() returns a board-state evaluation based only on piece values
+# @param board  a python-chess board object containing the board state to be
+#               evaluated.
+# @return       the evaluation of the board state, with positive values good
+#               for white and negative values good for black.
+def eval_countpieces(board):
+    # running total of our evaluation of this board state
+    score = 0
+
+    # Iterate over every square on the board
+    for square in chess.SQUARES:
+        # Get the piece that resides there
+        piece = board.piece_at(square)
+        # If the square is empty, ignore it
+        if piece == None:
+            continue
+        # Get the point value of that piece
+        piece_pts = piece_to_pts(piece)
+        # if the piece is a white piece, add its value. Otherwise, subtract it.
+        if piece.color:
+            score += piece_pts
+        else:
+            score -= piece_pts
+    # return the final score
+    return score
